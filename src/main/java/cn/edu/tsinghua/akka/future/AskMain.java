a65d81e6f8270d0710d13d0743c7139c5a2fd3d0
qiaojialingithub <646274302@qq.com>
2018-05-10 13:27:27 +0800

    add gitignore

--- src/main/java/cn/edu/tsinghua/akka/future/AskMain.java
+++ src/main/java/cn/edu/tsinghua/akka/future/AskMain.java
@@ -22,17 +22,18 @@ public class AskMain {
         ActorRef printActor = system.actorOf(Props.create(PrintActor.class), "PrintActor");
         ActorRef workerActor = system.actorOf(Props.create(WorkerActor.class), "WorkerActor");
 
-        //等等future返回
-        Future<Object> future = Patterns.ask(workerActor, 5, 4000);
+        //等等future返回，设置超时时间为4000ms
+        Future<Object> future = Patterns.ask(workerActor, 2, 4000);
         System.out.println("waiting");
         int result = (int) Await.result(future, Duration.create(4, TimeUnit.SECONDS));
         System.out.println("result:" + result);
 
-        //不等待返回值，直接重定向到其他actor，有返回值来的时候将会重定向到printActor
+        //不等待返回值，有返回值来的时候将会重定向到printActor
         Future<Object> future1 = Patterns.ask(workerActor, 8, 4000);
         Patterns.pipe(future1, system.dispatcher()).to(printActor);
         System.out.println("do not wait");
 
         workerActor.tell(PoisonPill.getInstance(), ActorRef.noSender());
+        system.terminate();
     }
 }
